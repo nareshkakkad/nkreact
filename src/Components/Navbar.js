35795fb0ce43c093c0290a953c40bf4d99d90362
nareshkakkad <naresh.kakkad@gmail.com>
2022-09-17 20:55:13 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,18 +6,18 @@ export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">{props.title}</a>
+    <a className="navbar-brand" href="https://nareshkakkad.github.io/nkreact/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item"><Link className="nav-link" to="https://nareshkakkad.github.com/nkreact/"> Home</Link> </li>
-        <li className="nav-item"> <Link className="nav-link" to="https://nareshkakkad.github.com/nkreact/about">About</Link>  </li>
-        <li className="nav-item"> <Link className="nav-link" to="https://nareshkakkad.github.com/nkreact/services">Services</Link>   </li>  
-        <li className="nav-item"> <Link className="nav-link" to="https://nareshkakkad.github.com/nkreact/form">Form</Link>   </li>  
-        <li className="nav-item"> <Link className="nav-link" to="https://nareshkakkad.github.com/nkreact/formlist">Form List</Link>   </li>  
+        <li className="nav-item"><Link className="nav-link" to="https://nareshkakkad.github.io/nkreact/"> Home</Link> </li>
+        <li className="nav-item"> <Link className="nav-link" to="https://nareshkakkad.github.io/nkreact/about">About</Link>  </li>
+        <li className="nav-item"> <Link className="nav-link" to="https://nareshkakkad.github.io/nkreact/services">Services</Link>   </li>  
+        <li className="nav-item"> <Link className="nav-link" to="https://nareshkakkad.github.io/nkreact/form">Form</Link>   </li>  
+        <li className="nav-item"> <Link className="nav-link" to="https://nareshkakkad.github.io/nkreact/formlist">Form List</Link>   </li>  
       </ul>
       
       {props.searchBar ?<form className="d-flex" role="search">
